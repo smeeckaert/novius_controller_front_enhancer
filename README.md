57aa6b98d330683a9ab24f280c86357bde846a40
Martin Smeeckaert <smeeckaert@novius.fr>
2015-03-05 15:47:01 +0100

    build route

--- README.md
+++ README.md
@@ -1,1 +1,167 @@
-# enhancer
+# Controller_Front_Application_Enhancer
+
+The Controller_Front_Application_Enhancer is an extension of \Nos\Controller_Front_Application which handle routes for urlEnhancers.
+
+
+## Setup
+
+Extend from \Enhancer\Controller_Front_Application_Enhancer
+
+```php
+class Controller_Front_MyEnhancer extends \Enhancer\Controller_Front_Application_Enhancer
+{
+...
+}
+```
+
+Make your enhancer point to the "route" action.
+
+```php
+    'enhancers'  => array(
+        'my_enhancer_name' => array(
+            'title'       => 'Enhancer With Route',
+            'desc'        => '',
+            'urlEnhancer' => 'application_name/front/myEnhancer/route', // URL of the enhancer
+        )
+    ),
+```
+
+### Declaring your routes
+
+Your routes are defined by an associative array pattern => action.
+
+The pattern is defined by segments separated by "/". If a segment begins by ":" it will be considered a [Parameters][].
+
+The action is the name of a public function named "action_{action}".
+
+```php
+protected static $_routes = array(
+        '/'                             => 'home',
+        '/:page'                        => 'page',
+        '/:char'                        => 'foo',
+        '/:id'                          => 'foo',
+        '/text/:page'                   => 'page',
+        '/:page/:id'                    => 'bar',
+        '/:page/:id/:char/have-fun'     => 'fun'
+    );
+```
+
+The route priority is defined by the order of the routes in this array. The first declared route will be matched first.
+(i.e : The /:page route will be matched before the /:char
+
+### [Parameters]
+
+Parameters are set in the protected static $_params property as an associative array ({param_name}:configuration)
+
+For each parameter in your routes you have to define an element and his configuration in this array.
+
+
+```php
+protected static $_params = array(
+        'id'   => array(),                                   // Empty configuration
+        'char' => array('match' => 'is_string'),             // Callable configuration
+        'page' => array('model' => '\Nos\Page\Model_Page'),  // Model configuration
+        'image' => array('model' => 'Model_Image', 'field' => 'image_searcheable_field')  // Model configuration
+
+    );
+```
+
+#### Parameter configuration
+
+In order to match parameters, you have to set a configuration as value of each parameter.
+
+If the configuration is empty, the parameter will always match like a wildcard.
+
+##### match
+
+The key 'match' can contain a callable that will be used to check the value of the parameter.
+
+##### model
+
+The key 'model' can contain a class name that will be used to find the property.
+
+If the parameter 'field' is not provided, it will try to find a VirtualPath or VirtualName behaviour and extract the correct field to match.
+
+You can give it classes which are not models but they must have a public static method find() with \Nos\Orm\Model prototyping.
+
+##### field
+
+The key 'field' contain the mysql column which will be used to retrieve the model.
+
+It's optionnal for models with VirtualPath or VirtualName behaviour.
+
+### Accessing parameters
+
+To access parameters from within an action, call the method routeParam().
+
+This method will return the matched parameter or null if it's not defined.
+
+```php
+public function action_page()
+{
+    $modelPage = $this->routeParam('page');
+}
+```
+
+### Building URLs
+
+To build automatically your urls, simply give at route url builder parameters as an associative array ({param_name}:value).
+
+The class will then build the most specialized url it could make.
+
+```php
+echo Tools_Enhancer::url('enhancer_test', array('page' => $page, 'id' => 'id-test', 'char' => 'b'));
+```
+
+Will output
+```
+http://.../enhancer_url/page-virtual-name/id-test/b/have-fun.html
+```
+
+Caution: The builder always assume that your parameters are correct.
+
+## Example
+
+```php
+
+class Controller_Front_MyEnhancer extends \Enhancer\Controller_Front_Application_Enhancer
+{
+    protected static $_routes = array(
+        '/'                             => 'home',
+        '/:page'                        => 'page',
+        '/:char'                        => 'foo',
+        '/:id'                          => 'foo',
+        '/text/:page'                   => 'page',
+        '/:page/:id'                    => 'bar',
+        '/:page/:id/:char/have-fun'     => 'fun'
+    );
+
+    protected static $_params = array(
+        'id'   => array(),                                   // Empty configuration
+        'char' => array('match' => 'is_string'),             // Callable configuration
+        'page' => array('model' => '\Nos\Page\Model_Page'),  // Model configuration
+    );
+
+    // Will be called by /
+    public function action_home(){
+    }
+
+    // Will be called by /virtual-name-page and /text/virtual-name-page
+    public function action_page(){
+    }
+
+    // Will be called by /[^virtual-name-page]
+    public function action_foo(){
+       $char = $this->routeParams('char');
+       var_dump($char); // Null if the route was not a string
+    }
+
+    // Will be called by /virtual-name-page/*
+    public function action_bar(){
+    }
+
+    // Will be called by /virtual-name-page/*/[A-Z]+/have-fun
+    public function action_fun(){
+    }
+}
+```
--- classes/controller/front/application/enhancer.ctrl.php
+++ classes/controller/front/application/enhancer.ctrl.php
@@ -4,24 +4,75 @@ namespace Enhancer;
 
 class Controller_Front_Application_Enhancer extends \Nos\Controller_Front_Application
 {
-    protected $_routes = array();
-    protected $_params = array();
-    private $_cacheRoute = null;
+    protected static $_routes = array();
+    protected static $_params = array();
+    protected static $_cacheRoute = null;
     private $_cacheParams = array();
+    protected static $_cacheProperty = null;
 
     const ROUTE_SEPARATOR = '/';
     const PARAM_SEMAPHOR  = ':';
+
+    /**
+     * Retrieve a route parameter
+     *
+     * @param $param name of the parameter
+     *
+     * @return null|mixed
+     */
+    protected function routeParam($param)
+    {
+        if (!isset($this->_cacheParams[$param])) {
+            return null;
+        }
+        return $this->_cacheParams[$param];
+    }
+
+
+    public static function getUrlEnhanced($params = array())
+    {
+        $bestRouteParameters = 0;
+        $bestRoute           = null;
+        // Find route with the more matching parameters
+        foreach (static::$_cacheRoute as $count => $cachedRoutes) {
+            foreach ($cachedRoutes as $key => $route) {
+                $match      = true;
+                $matchCount = 0;
+                // Try to match all parameters
+                foreach ($route['route'] as $param) {
+                    $extract = static::extractParameter($param);
+                    if (!empty($extract) && empty($params[$extract])) {
+                        $match = false;
+                        break;
+                    }
+                    if (!empty($extract)) {
+                        $matchCount++;
+                    }
+                }
+                // if we match the more parameter, keep this route
+                if ($match && $matchCount > $bestRouteParameters) {
+                    $bestRouteParameters = $matchCount;
+                    $bestRoute           = $route;
+                }
+            }
+        }
+        if (!empty($bestRoute)) {
+            return static::buildRoute($params, $bestRoute['route']);
+        }
+        return false;
+    }
+
 
     public function action_route()
     {
         $enhancer_url = $this->main_controller->getEnhancerUrl();
         $route        = $this->explodeRoute($enhancer_url);
-        $this->initCache();
+        static::initCache();
         $cArgs = count($route);
-        if (!isset($this->_cacheRoute[$cArgs])) {
+        if (!isset(static::$_cacheRoute[$cArgs])) {
             throw new \Nos\NotFoundException();
         }
-        $matchingRoute = $this->findMatchingRoutes($route, $this->_cacheRoute[$cArgs]);
+        $matchingRoute = $this->findMatchingRoutes($route, static::$_cacheRoute[$cArgs]);
         if (empty($matchingRoute)) {
             throw new \Nos\NotFoundException();
         }
@@ -29,6 +80,54 @@ class Controller_Front_Application_Enhancer extends \Nos\Controller_Front_Applic
         return $this->$action();
     }
 
+    /**
+     * Returns the name of a parameter or null if it's not a parameter
+     *
+     * @param $str
+     *
+     * @return null|string
+     */
+    private static function extractParameter($str)
+    {
+        return ($str[0] == Controller_Front_Application_Enhancer::PARAM_SEMAPHOR ? substr($str, 1) : null);
+    }
+
+    /**
+     * Build a an url according to a route and parameters
+     *
+     * @param $params
+     * @param $route
+     *
+     * @return string
+     */
+    protected static function buildRoute($params, $route)
+    {
+        static::initCacheProperty();
+        // Replace route parameters with values
+        $route = array_map(function ($i) use ($params) {
+            $extract = static::extractParameter($i);
+            if (!empty($extract)) {
+                if (isset(static::$_cacheProperty[$extract])) {
+                    $prop = static::$_cacheProperty[$extract];
+                    return $params[$extract]->$prop;
+                } else {
+                    return $params[$extract];
+                }
+            }
+            return $i;
+        }, $route);
+        return implode('/', $route) . '.html';
+    }
+
+
+    /**
+     * Find the first matching route
+     *
+     * @param $route
+     * @param $cachedRoutes
+     *
+     * @return null
+     */
     private function findMatchingRoutes($route, $cachedRoutes)
     {
         $matchingRoute = null;
@@ -42,16 +141,20 @@ class Controller_Front_Application_Enhancer extends \Nos\Controller_Front_Applic
         return $matchingRoute;
     }
 
-    private function extractParameter($str)
-    {
-        return ($str[0] == Controller_Front_Application_Enhancer::PARAM_SEMAPHOR ? substr($str, 1) : null);
-    }
 
+    /**
+     * Test if all arguments of the route are filled with good values
+     *
+     * @param $route
+     * @param $parameters
+     *
+     * @return array|bool
+     */
     private function testRoute($route, $parameters)
     {
         $cacheParams = array();
         foreach ($route as $key => $routeElement) {
-            $extractParam      = $this->extractParameter($routeElement);
+            $extractParam      = static::extractParameter($routeElement);
             $matchingParameter = $parameters[$key];
             if ($extractParam) {
                 $matchingElement = $this->testParam($extractParam, $matchingParameter);
@@ -66,14 +169,22 @@ class Controller_Front_Application_Enhancer extends \Nos\Controller_Front_Applic
         return $cacheParams;
     }
 
+    /**
+     * Test if a param match his configuration
+     *
+     * @param $param
+     * @param $value
+     *
+     * @return bool|null
+     * @throws \Exception
+     */
     private function testParam($param, $value)
     {
-        $paramsInfos = \Arr::merge($this->_params[$param]);
-        if (empty($paramsInfos)) {
-            return false;
+        $paramsInfos = static::$_params[$param];
+        if (empty($paramsInfos)) { // No params, we always match this parameter
+            return $value;
         }
-        if (isset($paramsInfos['match'])) {
-
+        if (isset($paramsInfos['match'])) { // Callable property to match the variable
             if (!is_callable($paramsInfos['match'])) {
                 throw new \Exception("Match parameter must be callable");
                 return null;
@@ -85,7 +196,7 @@ class Controller_Front_Application_Enhancer extends \Nos\Controller_Front_Applic
             }
         }
         if (isset($paramsInfos['model'])) {
-            $model = $this->findModel($paramsInfos, $value);
+            $model = $this->findModel($param, $paramsInfos, $value);
             if (!empty($model)) {
                 return $model;
             }
@@ -93,7 +204,17 @@ class Controller_Front_Application_Enhancer extends \Nos\Controller_Front_Applic
         return false;
     }
 
-    private function findModel($params, $value)
+    /**
+     * Try to find a model matching the route parameters
+     *
+     * @param $paramKey
+     * @param $params
+     * @param $value
+     *
+     * @return null|Model
+     * @throws \Exception
+     */
+    private function findModel($paramKey, $params, $value)
     {
         $model      = $params['model'];
         $field_name = null;
@@ -102,22 +223,8 @@ class Controller_Front_Application_Enhancer extends \Nos\Controller_Front_Applic
             throw new \Exception("Model must have a query method");
         }
 
-        if (isset($params['field'])) {
-            $field_name = $params['field'];
-        } else { // We try to find the virtual name by default
-            $behaviours = "{$model}::behaviours";
-            if (is_callable($behaviours)) {
-                $hasVirtualName = call_user_func($behaviours, 'Nos\Orm_Behaviour_Virtualname');
-                if ($hasVirtualName) {
-                    $field_name = $hasVirtualName['virtual_name_property'];
-                }
-                $hasVPath = call_user_func($behaviours, 'Nos\Orm_Behaviour_Virtualpath');
-                if ($hasVPath) {
-                    $field_name = $hasVPath['virtual_name_property'];
-                }
-
-            }
-        }
+        static::initCacheProperty();
+        $field_name = \Arr::get(static::$_cacheProperty, $paramKey);
         if (empty($field_name)) {
             return null;
         }
@@ -128,32 +235,70 @@ class Controller_Front_Application_Enhancer extends \Nos\Controller_Front_Applic
         return $query->where($field_name, $value)->get_one();
     }
 
-    protected function route_param($param)
+    /**
+     * Put in cache database property of models fields
+     * Will try VirtualName and VirtualPath enhancers if no fields are given
+     *
+     * Then the database field will be stored in $_cacheProperty[name]
+     */
+    protected static function initCacheProperty()
     {
-        if (!isset($this->_cacheParams[$param])) {
-            return null;
+        if (static::$_cacheProperty !== null) {
+            return;
         }
-        return $this->_cacheParams[$param];
+        foreach (static::$_params as $key => $params) {
+            $model      = \Arr::get($params, 'model');
+            $field_name = null;
+
+            if (isset($params['field'])) {
+                $field_name = $params['field'];
+            } elseif ($model) {
+                $behaviours     = "{$model}::behaviours";
+                $hasVirtualName = call_user_func($behaviours, 'Nos\Orm_Behaviour_Virtualname');
+                if ($hasVirtualName) {
+                    $field_name = $hasVirtualName['virtual_name_property'];
+                }
+                $hasVPath = call_user_func($behaviours, 'Nos\Orm_Behaviour_Virtualpath');
+                if ($hasVPath) {
+                    $field_name = $hasVPath['virtual_name_property'];
+                }
+            }
+            if (!empty($field_name)) {
+                static::$_cacheProperty[$key] = $field_name;
+            }
+        }
     }
 
-    private function explodeRoute($route)
+
+    /**
+     * Explode a route into segments
+     *
+     * @param $route
+     *
+     * @return array
+     */
+    private static function explodeRoute($route)
     {
         return array_values(array_filter(explode(Controller_Front_Application_Enhancer::ROUTE_SEPARATOR, $route)));
     }
 
-    private function initCache()
+    /**
+     * Init the cache route
+     * Will store routes by size in $_cacheRoute
+     */
+    private static function initCache()
     {
-        if ($this->_cacheRoute !== null) {
+        if (static::$_cacheRoute !== null) {
             return;
         }
-        $this->_cacheRoute = array();
-        foreach ($this->_routes as $route => $action) {
-            $route = $this->explodeRoute($route);
+        static::$_cacheRoute = array();
+        foreach (static::$_routes as $route => $action) {
+            $route = static::explodeRoute($route);
             $c     = count($route);
-            if (!isset($this->_cacheRoute[$c])) {
-                $this->_cacheRoute[$c] = array();
+            if (!isset(static::$_cacheRoute[$c])) {
+                static::$_cacheRoute[$c] = array();
             }
-            $this->_cacheRoute[$c][] = array('route' => $route, 'action' => $action);
+            static::$_cacheRoute[$c][] = array('route' => $route, 'action' => $action);
         }
     }
 }
